3a14b42e352b135f86fe01f939727a429498aa90
brian-boisjoli <boisjolibrian@gmail.com>
2024-02-08 12:45:21 -0500

    click to open

--- src/app/components/slider.tsx
+++ src/app/components/slider.tsx
@@ -1,14 +1,15 @@
 "use client";
-
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 export default function Slider() {
-  const scrollAmount = 100;
-  // Specify the type of elements the ref will refer to
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
   const sliderRef = useRef<HTMLDivElement>(null);
+  const scrollAmount = 100;
   const images = [
+    // Your images array...
     {
       id: 1,
       url: "https://i.postimg.cc/HnVsbwrv/botman1.jpg",
@@ -35,6 +36,15 @@ export default function Slider() {
     },
   ];
 
+  // Function to open the popup with the selected image
+  const openPopup = (imageUrl: string) => {
+    setSelectedImage(imageUrl);
+    setIsPopupOpen(true);
+  };
+
+  // Function to close the popup
+  const closePopup = () => setIsPopupOpen(false);
+
   return (
     <div className="App">
       <button
@@ -42,23 +52,22 @@ export default function Slider() {
         onClick={() => {
           const container = sliderRef.current;
           if (container) {
-            container.scrollLeft += scrollAmount;
+            container.scrollLeft -= scrollAmount;
           }
         }}
       >
         <ChevronLeftIcon />
       </button>
       <div className="images-container" ref={sliderRef}>
-        {images.map((image) => {
-          return (
-            <img
-              className="image"
-              alt="sliderImage"
-              key={image?.id}
-              src={image?.url}
-            />
-          );
-        })}
+        {images.map((image) => (
+          <img
+            className="image"
+            alt="sliderImage"
+            key={image.id}
+            src={image.url}
+            onClick={() => openPopup(image.url)} // Add onClick handler here
+          />
+        ))}
       </div>
       <button
         className="nav-btn"
@@ -71,6 +80,12 @@ export default function Slider() {
       >
         <ChevronRightIcon />
       </button>
+      {isPopupOpen && ( // Conditional rendering for the popup
+        <div className="image-popup" onClick={closePopup}>
+          <img src={selectedImage} alt="Selected" />
+          {/*<p>Click anywhere to close</p>*/}
+        </div>
+      )}
     </div>
   );
 }
