809b74621d5c15e158bb84838d2b726a1e35e684
brian-boisjoli <boisjolibrian@gmail.com>
2024-02-08 14:11:47 -0500

    updates

--- src/app/components/audio.tsx
+++ src/app/components/audio.tsx
@@ -1,27 +1,33 @@
 "use client";
-// AudioToggle.js
 import React, { useState, useEffect } from "react";
 
 const AudioToggle = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const audio = new Audio("/audio/batman_theme.mp3"); // Adjust the path as necessary
 
   useEffect(() => {
-    // Play or pause the audio based on the isPlaying state
-    isPlaying ? audio.play() : audio.pause();
+    const audio = new Audio("/audio/batman_theme.mp3"); // Adjust the path as necessary
+    // Function to play or pause the audio based on isPlaying state
+    const togglePlayback = () => {
+      if (isPlaying) {
+        audio
+          .play()
+          .catch((error) => console.error("Error playing audio:", error));
+      } else {
+        audio.pause();
+      }
+    };
+
+    // Call togglePlayback whenever isPlaying changes
+    togglePlayback();
 
     // Cleanup function to pause the audio when the component unmounts
     return () => {
       audio.pause();
     };
-  }, [isPlaying, audio]);
-
-  const togglePlay = () => {
-    setIsPlaying(!isPlaying);
-  };
+  }, [isPlaying]); // Depend on isPlaying to toggle playback
 
   return (
-    <button className="audio-toggle" onClick={togglePlay}>
+    <button className="audio-toggle" onClick={() => setIsPlaying(!isPlaying)}>
       {isPlaying ? "Pause" : "Play"}
     </button>
   );
