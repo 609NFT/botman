ef16d130d833b0b861aff9854554c5b3e411f036
brian-boisjoli <boisjolibrian@gmail.com>
2024-02-14 23:09:09 -0500

    typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // Adjust according to your project structure
+    './components/**/*.{js,ts,jsx,tsx}' // Adjust according to your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
