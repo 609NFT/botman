579ec1c3086bf3bb5c37a06d5737301f01021406
brian-boisjoli <boisjolibrian@gmail.com>
2024-02-17 08:43:59 -0500

    updates

--- src/data/charactersData.js
+++ src/data/charactersData.js
@@ -2,33 +2,33 @@
 const characters = [
     {
         name: 'Botman',
-        description: 'The one and only.',
+        description: 'Despite his unconventional methods and the challenges he faces in understanding the complexities of social cues and conventions, Botmans dedication to doing good and his genuine desire to help others shine through.',
         imageUrl: 'https://i.postimg.cc/3JR6bdjY/photo-2024-02-16-17-07-23.jpg',
       },
     {
-      name: 'Character Two',
-      description: 'This is a description of Character Two.',
+      name: 'Raychull',
+      description: 'Working within the legal system as an assistant district attorney, she tirelessly fights for joostice, embodying hope and moral integrity in a city overshadowed by crime and corruption.',
       imageUrl: 'https://i.postimg.cc/vZ4JDcD3/photo-2024-02-16-17-07-07.jpg',
     },
     {
-      name: 'Character Three',
-      description: 'This is a description of Character Three.',
+      name: 'Jokar',
+      description: 'Chaotically malevolent villain with a penchant for anarchic humor, relentlessly challenging Batman and Gotham City with his twisted vision of fun',
       imageUrl: 'https://i.postimg.cc/LXpwyDDb/photo-2024-02-16-17-07-10.jpg',
     },
     {
+      name: 'Bayn',
+      description: 'Formidable adversary of Botman, known for his immense strength, strategic genius, and the iconic use of the Venom drug to enhance his physical abilities.',
+      imageUrl: 'https://i.postimg.cc/m27w6y0D/photo-2024-02-12-17-03-35.jpg',
+    },
+    {
         name: 'Awlfwed',
-        description: 'This is a description of Awlfwed.',
+        description: 'Awlfwed Pennyworth is the loyal butler and confidant of Bruce Wayne, offering unwavering support and sage advice to Botman.',
         imageUrl: 'https://i.postimg.cc/6q6SKQDV/photo-2024-02-16-17-07-18.jpg',
       },
       {
         name: 'Glen',
-        description: 'This is a description of Glen.',
+        description: 'A normal man navigates the complexities of everyday life with a blend of resilience, routine, and the pursuit of simple joys amidst the mundane.',
         imageUrl: 'https://i.postimg.cc/1zxjtBk3/photo-2024-02-16-17-07-25.jpg',
-      },
-      {
-        name: 'Character six',
-        description: 'This is a description of character six.',
-        imageUrl: 'https://i.postimg.cc/m27w6y0D/photo-2024-02-12-17-03-35.jpg',
       },
     // Add more characters as needed
   ];
