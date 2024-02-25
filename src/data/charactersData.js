b07492b6973b90fe751572b13d3be257004a32b8
brian-boisjoli <boisjolibrian@gmail.com>
2024-02-25 09:19:38 -0500

    updates

--- src/data/charactersData.js
+++ src/data/charactersData.js
@@ -2,32 +2,32 @@
 const characters = [
     {
         name: 'Botman',
-        description: 'Despite his unconventional methods and the challenges he faces in understanding the complexities of social cues and conventions, Botmans dedication to doing good and his genuine desire to help others shine through.',
+        description: 'Once a hopeful investor in LunarSol, Glen Turings dreams were shattered alongside Sothams digital economy. Stricken with Autism, he donned the mantle of Botman, channelling his newfound eccentricities into a force for good.',
         imageUrl: 'https://i.postimg.cc/3JR6bdjY/photo-2024-02-16-17-07-23.jpg',
       },
     {
-      name: 'Raychull',
-      description: 'Working within the legal system as an assistant district attorney, she tirelessly fights for joostice, embodying hope and moral integrity in a city overshadowed by crime and corruption.',
+      name: 'Botwoman/Dr. Selena Cipher',
+      description: 'Before adopting the moniker of Botwoman, she was Dr. Selena Cipher, a brilliant cryptographer renowned for her groundbreaking work in blockchain security and her advocacy for digital privacy. Caught in the LunarSol collapse, her Autism manifested in a fierce pursuit of justice mixed with a flair for the dramatic. As Botwoman, she walks the fine line between ally and vigilante, using her tech-savvy skills to fight against Sothams digital criminals. Her complex relationship with Botman oscillates between collaboration and competition, united by a shared history but divided by methods.',
       imageUrl: 'https://i.postimg.cc/vZ4JDcD3/photo-2024-02-16-17-07-07.jpg',
     },
     {
-      name: 'Jokar',
-      description: 'Chaotically malevolent villain with a penchant for anarchic humor, relentlessly challenging Batman and Gotham City with his twisted vision of fun',
-      imageUrl: 'https://i.postimg.cc/LXpwyDDb/photo-2024-02-16-17-07-10.jpg',
+      name: 'Awlfred/David Pennypinch',
+      description: 'Known in the financial circles of Sotham as David Pennypinch, he was once a trusted financial advisor who prided himself on his ethical approach and deep understanding of the cryptocurrency market. He found himself amongst the victims of LunarSols deceit. The shock propelled him into a state of Autism, where he embraced a role as Sothams unlikeliest informant. Drawing from his vast knowledge of Sothams economic underbelly, Awlfred aids Botman with insider information, serving as the eyes and ears within places even Botman cant reach.',
+      imageUrl: 'https://i.postimg.cc/6q6SKQDV/photo-2024-02-16-17-07-18.jpg',
     },
     {
-      name: 'Bayn',
-      description: 'Formidable adversary of Botman, known for his immense strength, strategic genius, and the iconic use of the Venom drug to enhance his physical abilities.',
+      name: 'Bayn/Ethan Stronghold',
+      description: 'Prior to LunarSols rug pull, Ethan Stronghold was a security consultant with a reputation for his uncompromising standards and an unmatched track record in safeguarding digital assets against cyber threats. His career and personal savings were obliterated. Autism twisted his grief into rage, transforming him into Bayn. Now, he seeks to dismantle Sothams reliance on digital finance, viewing it as the root of societal decay. Bayn stands as Botmans most daunting adversary, challenging him with a brute force thats as unpredictable as it is relentless.',
       imageUrl: 'https://i.postimg.cc/m27w6y0D/photo-2024-02-12-17-03-35.jpg',
     },
     {
-        name: 'Awlfwed',
-        description: 'Awlfwed Pennyworth is the loyal butler and confidant of Bruce Wayne, offering unwavering support and sage advice to Botman.',
-        imageUrl: 'https://i.postimg.cc/6q6SKQDV/photo-2024-02-16-17-07-18.jpg',
-      },
+      name: 'Joker/Jasper Arkham',
+      description: 'The Joker once operated under his real name, Jasper Arkham, a visionary tech entrepreneur whose innovative ideas and charismatic leadership made him a leading figure in Sothams tech revolution. Jasper saw his empire crumble in the wake of LunarSols collapse. Stricken by Autism, his genius took a dark turn, breeding a chaotic force driven by a twisted sense of humor and revenge. He sees the digital world as his playground, using his deep understanding of technology to orchestrate elaborate schemes that keep Sotham on its toes.',
+      imageUrl: 'https://i.postimg.cc/LXpwyDDb/photo-2024-02-16-17-07-10.jpg',
+    },
       {
-        name: 'Glen',
-        description: 'A normal man navigates the complexities of everyday life with a blend of resilience, routine, and the pursuit of simple joys amidst the mundane.',
+        name: 'Glen Timpson',
+        description: 'Once a hopeful investor in LunarSol, Glen Turings dreams were shattered alongside Sothams digital economy. Stricken with Autism, he donned the mantle of Botman, channelling his newfound eccentricities into a force for good.',
         imageUrl: 'https://i.postimg.cc/1zxjtBk3/photo-2024-02-16-17-07-25.jpg',
       },
     // Add more characters as needed
