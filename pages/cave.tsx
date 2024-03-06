62847fd7155f4076cb0d48dfe478cc97be906c75
609NFT <boisjolibrian@gmail.com>
2024-03-06 14:18:32 -0500

    game

--- pages/cave.tsx
+++ pages/cave.tsx
@@ -4,6 +4,7 @@ import Navigation from "../src/app/components/navigation";
 import GameModal from "@/app/components/capedCrusader";
 import RacerModal from "@/app/components/racer";
 import Game1Modal from "@/app/components/game1";
+import Game2Modal from "@/app/components/game2";
 import { Row, Col, TabContainer } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -25,6 +26,17 @@ export default function Play() {
           </Col>
           <Col sm={4}>
             <Game1Modal></Game1Modal>
+          </Col>
+        </Row>
+        <Row className="games">
+          <Col sm={4}>
+            <Game2Modal></Game2Modal>
+          </Col>
+          <Col sm={4}>
+            <div></div>
+          </Col>
+          <Col sm={4}>
+            <div></div>
           </Col>
         </Row>
       </TabContainer>
