eab2d0549c0dfa2ecda4f1352f957b06c0391b38
609NFT <boisjolibrian@gmail.com>
2024-03-28 14:52:40 -0400

    DC

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -13,9 +13,18 @@ const Footer: React.FC = () => {
     <footer style={footerStyle}>
       <p>© {new Date().getFullYear()} Botman. All rights reserved.</p>
       <div>
-        $BOTMAN WAS MADE FOR FUN AND HAS NO VALUE. DONT RISK MONEY YOU ARE
-        AFRAID OF LOSING. THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT
-        RESPONSIBLE FOR THE PRICE OF THE TOKEN.
+        Disclaimer: $BOTMAN WAS MADE FOR FUN AND HAS NO VALUE. DONT RISK MONEY
+        YOU ARE AFRAID OF LOSING. THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE
+        NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN. Botman memecoin is an
+        independent digital currency project and is not affiliated, associated,
+        authorized, endorsed by, or in any way officially connected with DC
+        Comics, Warner Bros., or any of their subsidiaries or affiliates. The
+        official DC Comics website can be found at{" "}
+        <a target="_blank" href="https://www.dc.com/">
+          DC.com
+        </a>
+        . The name Batman as well as related names, marks, emblems, and images
+        are registered trademarks of their respective owners.
       </div>
     </footer>
   );
